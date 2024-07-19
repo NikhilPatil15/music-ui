fce66c8782d8421b1fa56bea02b39efad04116af
NikhilPatil15 <137986217+NikhilPatil15@users.noreply.github.com>
2024-07-20 04:01:01 +0530

    sticky-scrollbar done

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -20,7 +20,7 @@ export default function FeaturedSection() {
     (course: Course) => course.isFeatured
   );
 
-  return (
+  return  (
     <div className="py-12 bg-gray-900">
       <div className="text-center">
         <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">
